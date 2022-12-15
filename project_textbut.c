f4f4e24ab74f08ec82bf5bd9b652a1427791b15c
Soeun Park <sr01203@naver.com>
2022-12-15 16:33:01 +0900

    projecttttttttt
    c total file

--- project_textbut.c
+++ project_textbut.c
@@ -2,6 +2,7 @@
 #include "button.h"
 #include "led.h"
 #include "fnd.h"
+#include "touch.h"
 int main(void)
 {
    unsigned int linenum = 0;
@@ -9,7 +10,6 @@ int main(void)
    int keycheck;
    int fd;
    int len;
-   int i = 0;
    int number;
    memset(&stlcd, 0, sizeof(stTextLCD));
    lcdtextwrite("===== MENU =====", 0, 1);
@@ -23,20 +23,26 @@ int main(void)
 
    printf("message get success\r\n");
 
-
    ledLibInit();
 
    // 초기화
-   ledOnOff(0,0);
-   ledOnOff(1,0);
-   ledOnOff(2,0);
-   ledOnOff(3,0);
-   ledOnOff(4,0);
-   ledOnOff(5,0);
-   ledOnOff(6,0);
+   ledOnOff(0, 0);
+   ledOnOff(1, 0);
+   ledOnOff(2, 0);
+   ledOnOff(3, 0);
+   ledOnOff(4, 0);
+   ledOnOff(5, 0);
+   ledOnOff(6, 0);
 
    buttonInit();
-   
+   // touch
+   touchInit();
+   TOUCH_MSG_T recv;
+
+   char *buf[100][200];
+   int i = 0;
+   int k = 0;
+
    // printf("button start2 - case\r\n");
    // printf("pwmleditit start\r\n");
    // pwmLedInit();
@@ -57,24 +63,25 @@ int main(void)
 
    // printf("pwmincativeall stop\r\n");
    // printf("\r\n");
-   //fnd
+   // fnd
    typedef struct tm_t
-        {
-            int tm_hour;
-            int tm_min;
-            int tm_sec;
-        } TM_T;
-        //struct tm_t * ptmcur;
-        TM_T *ptmcur;
+   {
+      int tm_hour;
+      int tm_min;
+      int tm_sec;
+   } TM_T;
+   // struct tm_t * ptmcur;
+   TM_T *ptmcur;
 
-        time_t tTime;
-
+   time_t tTime;
 
    while (1)
    {
 
       msgrcv(msgID, &rcv.keyInput, sizeof(rcv.keyInput), 0, 0);
       printf("receive the MESSAGE ! \r\n");
+      msgrcv(msgID, &recv, sizeof(recv) - sizeof(long int), 0, 0);
+      printf("receive the touch message!\r\n");
 
       switch (rcv.keyInput)
       {
@@ -83,24 +90,24 @@ int main(void)
          printf("\r\nTEXTLCD -  PIANO/DRUM/GUITAR \r\n");
          lcdtextwrite("      PIANO     ", 0, 1);
          lcdtextwrite(0, " DRUM    GUITAR ", 2);
-         ledOnOff(1,1);
-         ledOnOff(2,0);
-         ledOnOff(3,0);
-         ledOnOff(4,0);
-         ledOnOff(5,0);
-         ledOnOff(6,0);
+         ledOnOff(1, 1);
+         ledOnOff(2, 0);
+         ledOnOff(3, 0);
+         ledOnOff(4, 0);
+         ledOnOff(5, 0);
+         ledOnOff(6, 0);
          // color LED
          //  pwmSetPercent(100,i);
          //  if(i==2) i = 0;
          //  else i = i+1;
-         //fnd
+         // fnd
          ptmcur = localtime(&tTime);
 
-                     number = ptmcur -> tm_hour * 10000;
-                     number += ptmcur ->tm_min *100;
-                     number += ptmcur -> tm_sec;
+         number = ptmcur->tm_hour * 10000;
+         number += ptmcur->tm_min * 100;
+         number += ptmcur->tm_sec;
 
-                     fndDisp(number,0b1010);
+         fndDisp(number, 0b1010);
 
          keycheck = 1;
          break;
@@ -109,12 +116,12 @@ int main(void)
          printf("\r\nTEXTLCD - UP\r\n");
          lcdtextwrite("LINE 1       O  ", 0, 1);
          lcdtextwrite(0, "LINE 2          ", 2);
-         ledOnOff(1,1);
-         ledOnOff(2,1);
-         ledOnOff(3,0);
-         ledOnOff(4,0);
-         ledOnOff(5,0);
-         ledOnOff(6,0);
+         ledOnOff(1, 1);
+         ledOnOff(2, 1);
+         ledOnOff(3, 0);
+         ledOnOff(4, 0);
+         ledOnOff(5, 0);
+         ledOnOff(6, 0);
          keycheck = 2;
          break;
 
@@ -122,12 +129,12 @@ int main(void)
          printf("\r\nTEXTLCD -  DOWN \r\n");
          lcdtextwrite("LINE 1          ", 0, 1);
          lcdtextwrite(0, "LINE 2       O  ", 2);
-         ledOnOff(1,1);
-         ledOnOff(2,1);
-         ledOnOff(3,1);
-         ledOnOff(4,0);
-         ledOnOff(5,0);
-         ledOnOff(6,0);
+         ledOnOff(1, 1);
+         ledOnOff(2, 1);
+         ledOnOff(3, 1);
+         ledOnOff(4, 0);
+         ledOnOff(5, 0);
+         ledOnOff(6, 0);
          keycheck = 3;
          break;
 
@@ -143,12 +150,12 @@ int main(void)
             lcdtextwrite("-----SELECT-----", 0, 1);
             lcdtextwrite(0, " LINE 2 SELECT  ", 2);
          }
-         ledOnOff(1,1);
-         ledOnOff(2,1);
-         ledOnOff(3,1);
-         ledOnOff(4,1);
-         ledOnOff(5,0);
-         ledOnOff(6,0);
+         ledOnOff(1, 1);
+         ledOnOff(2, 1);
+         ledOnOff(3, 1);
+         ledOnOff(4, 1);
+         ledOnOff(5, 0);
+         ledOnOff(6, 0);
          keycheck = 4;
          break;
 
@@ -158,28 +165,138 @@ int main(void)
             lcdtextwrite("      BACK      ", 0, 1);
             lcdtextwrite(0, "                ", 2);
          }
-         ledOnOff(1,1);
-         ledOnOff(2,1);
-         ledOnOff(3,1);
-         ledOnOff(4,1);
-         ledOnOff(5,1);
-         ledOnOff(6,0);
+         ledOnOff(1, 1);
+         ledOnOff(2, 1);
+         ledOnOff(3, 1);
+         ledOnOff(4, 1);
+         ledOnOff(5, 1);
+         ledOnOff(6, 0);
          keycheck = 5;
          break;
       case (6):
          printf("\r\nTEXTLCD - NONE\r\n");
-         ledOnOff(1,1);
-         ledOnOff(2,1);
-         ledOnOff(3,1);
-         ledOnOff(4,1);
-         ledOnOff(5,1);
-         ledOnOff(6,1);
+         ledOnOff(1, 1);
+         ledOnOff(2, 1);
+         ledOnOff(3, 1);
+         ledOnOff(4, 1);
+         ledOnOff(5, 1);
+         ledOnOff(6, 1);
          keycheck = 6;
          break;
       }
    }
 
-   buttonExit();
-   ledLibExit();
-   return 0;
+   switch (recv.keyInput)
+   {
+   case 999:
+      if (recv.pressed == 1)
+      {
+         if (recv.x >= 0 && recv.x < 205 && recv.y >= 401 && recv.y < 601)
+         {
+            system("aplay ./pianodo");
+            sprintf(buf[i], "aplay ./pianodo");
+         }
+
+         else if (recv.x >= 0 && recv.x < 205 && recv.y >= 201 && recv.y < 401)
+         {
+            system("aplay ./pianore");
+            sprintf(buf[i], "aplay ./pianore");
+         }
+
+         else if (recv.x >= 0 && recv.x < 205 && recv.y >= 0 && recv.y < 201)
+         {
+            system("aplay ./pianomi");
+            sprintf(buf[i], "aplay ./pianomi");
+         }
+
+         else if (recv.x >= 205 && recv.x < 410 && recv.y >= 401 && recv.y < 601)
+         {
+            system("aplay ./pianofa");
+            sprintf(buf[i], "aplay ./pianofa");
+         }
+
+         else if (recv.x >= 205 && recv.x < 410 && recv.y >= 201 && recv.y < 401)
+         {
+            system("aplay ./pianosol");
+            sprintf(buf[i], "aplay ./pianosol");
+         }
+
+         else if (recv.x >= 205 && recv.x < 410 && recv.y >= 0 && recv.y < 201)
+         {
+
+            system("aplay ./pianora");
+            sprintf(buf[i], "aplay ./pianora");
+         }
+
+         else if (recv.x >= 410 && recv.x < 615 && recv.y >= 401 && recv.y < 601)
+         {
+
+            system("aplay ./pianosi");
+            sprintf(buf[i], "aplay ./pianosi");
+         }
+
+         else if (recv.x >= 410 && recv.x < 615 && recv.y >= 201 && recv.y < 401)
+         {
+            system("aplay ./pianohighdo");
+            sprintf(buf[i], "aplay ./pianohighdo");
+         }
+
+         else if (recv.x >= 410 && recv.x < 615 && recv.y >= 0 && recv.y < 201)
+         {
+            system("aplay ./pianohighre");
+            sprintf(buf[i], "aplay ./pianohighre");
+         }
+
+         else if (recv.x >= 615 && recv.x < 820 && recv.y >= 401 && recv.y < 601)
+         {
+            system("aplay ./pianohighmi");
+            sprintf(buf[i], "aplay ./pianohighmi");
+         }
+
+         else if (recv.x >= 615 && recv.x < 820 && recv.y >= 201 && recv.y < 401)
+         {
+            system("aplay ./pianohighfa");
+            sprintf(buf[i], "aplay ./pianohighfa");
+         }
+
+         else if (recv.x >= 615 && recv.x < 820 && recv.y >= 0 && recv.y < 201)
+         {
+            system("aplay ./pianohighsol");
+            sprintf(buf[i], "aplay ./pianohighsol");
+         }
+
+         else if (recv.x >= 819 && recv.x < 1025 && recv.y >= 401 && recv.y < 601)
+         {
+            system("aplay ./pianohighra");
+            sprintf(buf[i], "aplay ./pianohighra");
+         }
+
+         else if (recv.x >= 819 && recv.x < 1025 && recv.y >= 201 && recv.y < 401)
+         {
+            for (k = 0; k <= i; k++)
+            {
+
+               system(buf[k]);
+               printf("system %d\r\n", k);
+            }
+            // system("aplay ./pianohighsi");
+            // sprintf(buf[i],"aplay ./pianohighsi");
+         }
+
+         else if (recv.x >= 819 && recv.x < 1025 && recv.y >= 0 && recv.y < 201) // 버퍼값 초기화.
+         {
+            // system("aplay ./pianohighhighdo");
+            for (k = 0; k <= i; k++)
+            {
+               memset(buf, 0x00, sizeof(buf)); // str 이라는 변수안의 값을 모두 0x00( 널 ) 으로 초기화
+            }
+         }
+         i++;
+         break;
+      }
+
+      buttonExit();
+      ledLibExit();
+      return 0;
+   }
 }
